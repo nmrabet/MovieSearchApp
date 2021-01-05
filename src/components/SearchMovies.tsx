8cd0a64bc05ba7e5e3d11b770631819b9b377929
Nanouss <mrabet_nesrine@yahoo.fr>
2021-01-05 12:09:43 +0100

    Separating movie card into its own component

--- src/components/SearchMovies.tsx
+++ src/components/SearchMovies.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 import { Movies, ListMovies } from "../types/index";
 
 export default function SearchMovies() {
@@ -41,23 +42,7 @@ export default function SearchMovies() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className='card' key={movie.id}>
-              <img
-                className='card--image'
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className='card--content'>
-                <h3 className='card--title'>{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className='card--desc'>{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
